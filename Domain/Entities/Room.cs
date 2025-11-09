379efe220156da962b91245d36d0000f1ec68e67
Range18 <shakmate@mail.ru>
2025-11-09 19:54:09 +0500

    Update Room

--- Domain/Entities/Room.cs
+++ Domain/Entities/Room.cs
@@ -5,23 +5,108 @@ namespace Domain.Entities;
 
 public class Room : IEntity<Guid>
 {
-    public Guid Id { get; }
-    public IReadOnlyList<Game> Games => games.AsReadOnly();
-    public HashSet<Player> Players => new();
+    public Guid Id { get; private set; }
+    public RoomPrivacy Privacy { get; private set; }
+    public string Code { get; private set; } = string.Empty;
 
-    private readonly List<Game> games = [];
+    public RoomStatus Status { get; private set; }
 
-    public Room()
+    private int _maxPlayers;
+
+    public int MaxPlayers
+    {
+        get => _maxPlayers;
+        set
+        {
+            if (Status == RoomStatus.Archived)
+                throw new InvalidOperationException("Комната архивирована");
+            if (HasOngoingGame())
+                throw new InvalidOperationException("В комнате сейчас идет игра, нельзя изменять настройки комнаты");
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(value);
+            _maxPlayers = value;
+        }
+    }
+
+    public IReadOnlyCollection<Game> Games => games.AsReadOnly();
+    public IReadOnlyCollection<Player> Players => players;
+
+    private readonly List<Game> games = new();
+    private readonly HashSet<Player> players = new();
+
+    protected Room()
+    {
+    }
+
+    public Room(RoomPrivacy privacy, string code, int maxPlayers)
     {
         Id = Guid.NewGuid();
+        Privacy = privacy;
+        Status = RoomStatus.Available;
+        Code = !string.IsNullOrWhiteSpace(code)
+            ? code
+            : throw new ArgumentException("При создании комнаты нужен код для подключения", nameof(code));
+        MaxPlayers = maxPlayers;
     }
-    
+
     public void AddGame(Game game)
-    { 
-        if (games[^1].Status is GameStatus.Finished)
-            games.Add(game);
-        throw new InvalidOperationException($"Нельзя начать новую игру. Последняя игра еще не окончена {games[^1].Status}");
+    {
+        if (Status == RoomStatus.Archived)
+            throw new InvalidOperationException("Комната архивирована");
+        if (HasOngoingGame())
+            throw new InvalidOperationException("Нельзя начать новую игру. Последняя игра еще не окончена");
+        ArgumentNullException.ThrowIfNull(game);
+
+        games.Add(game);
     }
-        
-    public void AddPlayer(Player player) => Players.Add(player);
+
+    public void AddPlayer(Player player)
+    {
+        if (Status == RoomStatus.Archived)
+            throw new InvalidOperationException("Комната архивирована");
+
+        ArgumentNullException.ThrowIfNull(player);
+
+        if (players.Contains(player))
+            return;
+
+        if (players.Count >= MaxPlayers)
+            throw new InvalidOperationException("Комната заполнена");
+
+        players.Add(player);
+    }
+
+    public void RemovePlayer(Player player)
+    {
+        if (Status == RoomStatus.Archived)
+            throw new InvalidOperationException("Комната архивирована");
+        ArgumentNullException.ThrowIfNull(player);
+        players.Remove(player);
+    }
+
+    public void ArchiveRoom()
+    {
+        Status = RoomStatus.Archived;
+        players.Clear();
+    }
+
+    public bool IsFull()
+    {
+        return games.Count == MaxPlayers;
+    }
+
+    public Game? CurrentGame()
+    {
+        if (Status == RoomStatus.Archived)
+            throw new InvalidOperationException("Комната архивирована");
+        return Games.LastOrDefault();
+    }
+
+    public bool HasOngoingGame()
+    {
+        if (Status == RoomStatus.Archived)
+            throw new InvalidOperationException("Комната архивирована");
+        var currentGame = CurrentGame();
+        if (currentGame is null) return false;
+        return currentGame.Status == GameStatus.InProgress;
+    }
 }
---
+++ Domain/Enums/RoomPrivacy.cs
@@ -0,0 +1,7 @@
+namespace Domain.Enums;
+
+public enum RoomPrivacy
+{
+    Public,
+    Private,
+}
