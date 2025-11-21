f334c118f8fce28567d775d21f1efdbc75d90ffb
Barmin_George <183339070+BarminGeorge@users.noreply.github.com>
2025-11-21 11:34:22 +0500

    app hub

---
+++ Application/Hubs/AppHub.cs
@@ -0,0 +1,14 @@
+using Application.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Application.Hubs;
+
+public partial class AppHub(IGameManager gameManager, IRoomManager roomManager) 
+    : Hub
+{
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await roomManager.HandleUserError(Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
+    }
+}
