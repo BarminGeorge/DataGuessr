a960f8b590b061b44b5544ff66a68b35d37034d5
sigmabasket <zikov0806@mail.ru>
2025-11-25 21:49:22 +0500

    Delate IRepository

--- Infrastructure/Interfaces/IUserRepository.cs
+++ Infrastructure/Interfaces/IUserRepository.cs
@@ -3,7 +3,7 @@ using Domain.Entities;
 
 namespace Infrastructure.Interfaces;
 
-public interface IUserRepository : IRepository<User>
+public interface IUserRepository
 {
     Task<OperationResult> AddAsync(User user, CancellationToken ct = default);
     Task<OperationResult<User>> GetByNameAsync(string userName, CancellationToken ct = default);
