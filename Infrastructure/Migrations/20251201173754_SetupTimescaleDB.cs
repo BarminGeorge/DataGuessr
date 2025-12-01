5b6db6894d125e7387754df0e41b33fc0982942f
sigmabasket <zikov0806@mail.ru>
2025-12-01 22:53:07 +0500

    добавил файл для переноса на сервер

---
+++ Infrastructure/Migrations/20251201173754_SetupTimescaleDB.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+public partial class SetupTimescaleDB : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        // Установить расширение
+        migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS timescaledb;");
+
+        // Удалить индексы (потому что они конфликтуют с гипертаблицей)
+        migrationBuilder.Sql("DROP INDEX IF EXISTS \"IX_rooms_Status\";");
+        migrationBuilder.Sql("DROP INDEX IF EXISTS \"IX_rooms_ClosedAt\";");
+
+        // Создать гипертаблицу
+        migrationBuilder.Sql(@"
+            SELECT create_hypertable('rooms', 'Id', 
+                if_not_exists => TRUE,
+                migrate_data => TRUE
+            );
+        ");
+
+        // Retention policy
+        migrationBuilder.Sql(@"
+            SELECT add_retention_policy('rooms', 
+                INTERVAL '30 days',
+                if_not_exists => TRUE
+            );
+        ");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("SELECT remove_retention_policy('rooms', if_exists => TRUE);");
+    }
+}
