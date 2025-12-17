9176175b8d80f56d11213f4a5a52d0fc4f3026e5
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2025-12-17 18:57:46 +0500

    migrate files

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca01d0feec649c94ac7b50840bf254f5973167b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1b97cec26629cf04ad82a639f8f6dea0ad6b06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
