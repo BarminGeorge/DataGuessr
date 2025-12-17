e48cf60921fe04140c3a1aed63d9fb2a8154bd33
Barmin_George <183339070+BarminGeorge@users.noreply.github.com>
2025-12-17 20:02:15 +0500

    remove somethink

--- Application/DtoUI/AnswerDto.cs
+++ Application/DtoUI/AnswerDto.cs
@@ -1,20 +1,10 @@
-using System.Text.Json.Serialization;
-
 namespace Application.DtoUI;
 
-[JsonPolymorphic(TypeDiscriminatorPropertyName = "$type")]
-[JsonDerivedType(typeof(DateTimeAnswerDto), typeDiscriminator: "datetime")]
-[JsonDerivedType(typeof(BoolAnswerDto), typeDiscriminator: "bool")]
 public abstract record AnswerDto;
 
-public record DateTimeAnswerDto([property: JsonPropertyName("value")] int Year) : AnswerDto
-{
-    public DateTimeAnswerDto(DateTime dateTime) : this(dateTime.Year)
-    {
-    }
-}
+public record DateTimeAnswerDto(int Year) : AnswerDto;
 
-public record BoolAnswerDto([property: JsonPropertyName("value")] int NumericValue) : AnswerDto
+public record BoolAnswerDto(int NumericValue) : AnswerDto
 {
     public BoolAnswerDto(bool boolValue) : this(boolValue ? 1 : 0)
     {
