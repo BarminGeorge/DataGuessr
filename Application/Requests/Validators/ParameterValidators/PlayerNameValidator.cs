d3df8ce1c6b1a86fd39deda72fd59552de64df82
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2025-12-18 12:46:22 +0500

    update krest and add russian letters

--- Application/Requests/Validators/ParameterValidators/PlayerNameValidator.cs
+++ Application/Requests/Validators/ParameterValidators/PlayerNameValidator.cs
@@ -10,6 +10,6 @@ public class PlayerNameValidator : AbstractValidator<string>
             .NotEmpty().WithMessage("Player name is required")
             .MinimumLength(2).WithMessage("Player name must be at least 2 characters long")
             .MaximumLength(30).WithMessage("Player name cannot exceed 30 characters")
-            .Matches(@"^[a-zA-Z0-9_\s]+$").WithMessage("Player name can only contain letters, numbers, spaces and underscores");
+            .Matches(@"^[а-яА-Яa-zA-Z0-9_\s]+$").WithMessage("Player name can only contain letters, numbers, spaces and underscores");
     }
 }
